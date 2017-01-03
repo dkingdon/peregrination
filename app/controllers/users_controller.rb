10527a63ce612ec0016102eee883894ac4004621
yasuper8 <ysuper8@gmail.com>
2017-01-03 12:28:42 -0800

    signup page, login page, and routing setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,32 @@
 class UsersController < ApplicationController
 
+  before_filter :authorize, except: [:index]
+
   def index
-    
+
   end
+
+  def show
+    @user = User.find_by_id(parmas[:id])
+  end
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :current_city, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
 
-root to:'users#index'
 
 get '/', to: 'users#index', as: 'users'
+
+
+get '/signup', to: 'users#new'
+post '/users', to: 'users#create'
+
+get '/users/id', to: 'users#show'
+
+
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+get  '/logout', to: 'sessions#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
