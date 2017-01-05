17fe7675044e603c79687e2fc88e0dbad58bf6a9
yasuper8 <ysuper8@gmail.com>
2017-01-05 15:49:30 -0800

    changed images url for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+City.destroy_all
+
 City.create({name: "San Francisco", image: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/08/SANFRANCISCO-1200x618.jpg"})
-City.create({name: "New York", image: "http://az616578.vo.msecnd.net/files/2016/04/27/635973353399336445-1421152854_NYC-1.jpg"})
+City.create({name: "New York", image: "https://static.mgmresorts.com/content/dam/MGM/new-york-new-york/architecture/exterior/skyline/new-york-new-york-skyline-angle-blue-background.tif.image.1440.550.high.jpg"})
 City.create({name: "Seattle", image: "https://cdn-payscale.com/content/wikipedia/commons/2/2f/Space_Needle002.jpg"})
-City.create({name: "London", image: "http://www.universal-tourguide.com/wp-content/uploads/2016/09/discoverlondon.jpg"})
+City.create({name: "London", image: "https://www.imperial.ac.uk/ImageCropToolT4/imageTool/uploaded-images/LONDON_shutterstock_229478404--tojpeg_1417791048879_x1.jpg"})
 City.create({name: "Gibraltar", image: "https://image.jimcdn.com/app/cms/image/transf/none/path/sf778462bcdf82c75/image/i8791725d74c556bc/version/1452438276/image.jpg"})
 City.create({name: "San Juan", image: "https://media.timeout.com/images/102852687/image.jpg"})
-City.create({name: "Tokyo", image: "http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/tokyo-mud-bath-bar-mudbath0716.jpg?itok=dJ8lDXJh"})
-City.create({name: "Paris", image: "http://images.huffingtonpost.com/2015-11-19-1447968585-1661590-6672156239_89c77d53d8_o.jpg"})
+City.create({name: "Tokyo", image: "https://www.omm.com/~/media/images/site/locations/tokyo_780x520px.ashx"})
+City.create({name: "Paris", image: "https://www.cpp.edu/~international/study-abroad/img/jXMx4mQz6MsnhP1FSKp2TJMt.jpeg"})
