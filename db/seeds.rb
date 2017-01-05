de59d19a3727a3becf2f2149b92621e93464080f
yasuper8 <ysuper8@gmail.com>
2017-01-05 14:20:38 -0800

    add home page formatting video

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create({title: "SF city", body: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."})
+
+City.create({name: "San Francisco", image: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/08/SANFRANCISCO-1200x618.jpg"})
+City.create({name: "New York", image: "http://az616578.vo.msecnd.net/files/2016/04/27/635973353399336445-1421152854_NYC-1.jpg"})
+City.create({name: "Seattle", image: "https://cdn-payscale.com/content/wikipedia/commons/2/2f/Space_Needle002.jpg"})
+City.create({name: "London", image: "http://www.universal-tourguide.com/wp-content/uploads/2016/09/discoverlondon.jpg"})
+City.create({name: "Gibraltar", image: "https://image.jimcdn.com/app/cms/image/transf/none/path/sf778462bcdf82c75/image/i8791725d74c556bc/version/1452438276/image.jpg"})
+City.create({name: "San Juan", image: "https://media.timeout.com/images/102852687/image.jpg"})
+City.create({name: "Tokyo", image: "http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/tokyo-mud-bath-bar-mudbath0716.jpg?itok=dJ8lDXJh"})
+City.create({name: "Paris", image: "http://images.huffingtonpost.com/2015-11-19-1447968585-1661590-6672156239_89c77d53d8_o.jpg"})
